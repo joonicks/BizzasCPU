c6eec180e498689a387ae6819277ca4cf670558a
joonicks <god@joonicks.eu>
2021-05-27 15:47:59 +0200

    SHR/SHL/RCR/RCL opcode length fix, CMP flag fix

--- quartus/13.0/controlunit.vhd
+++ quartus/13.0/controlunit.vhd
@@ -26,8 +26,8 @@ port(
 	);
 end controlunit;
 
--- Logic units (whole design/Cyclone II): 300 299 298 292 291 290 265 271 286 285 282 276 267 265 263 258 300 305 301 313
--- Logic units (control unit/Cyclone II): 52 50 53 51 58 59 50 52 51 49 58 67 74 71
+-- Logic units (whole design/Cyclone II): 300 299 298 292 291 290 265 271 286 285 282 276 267 265 263 258 300 305 301 312
+-- Logic units (control unit/Cyclone II): 52 50 53 51 58 59 50 52 51 49 58 67 74 70
 
 architecture arch of controlunit is
 signal irop:			std_logic_vector(7 downto 0);
@@ -68,7 +68,7 @@ begin
 				when 16 to 23				=> nextc := 3; -- LD/ST imm16
 				when 0 to 7 | 24 to 63	=> nextc := 2; -- JMP imm16, LD/ST imm8 zeropage
 				when 8 to 15				=> nextc := 1; -- JMP rel8, LD/ST [imm8:reg]
-				when 104 to 127			=> nextc := 0; -- IND, DEC, XOR
+				when 64 to 127				=> nextc := 0; -- SHR, SHL, RCR, RCL, INC, DEC, XOR and unallocated opcodes
 				when others					=> null;			-- ALU ops (1 if samereg, else 0)
 				end case;
 				mcycle <= "0001";
@@ -180,25 +180,25 @@ begin
 				ALU_OP		<= "11" & irop(2);
 				ALU_Cin		<= irop(3) and F_Carry;
 				F_Store		<= '1';
-				Bus2Dst		<= "01"; -- "01" ALUBus2Dst
+				Bus2Dst		<= "01";  -- "01"  ALUBus2Dst
 			when 64 to 79 | 96 to 103 => null;
 			when 104 to 111 =>
 				-- 011010YY INC reg
 				-- 011011YY DEC reg
 				ALU_OP		<= irop(2) & "00";
 				F_Store		<= c0;
-				ModSel		<= "110"; -- "110": ONE constant
-				Bus2Dst(1)	<= '0';
-				Bus2Dst(0)	<= '1'; -- "01" ALUBus2Dst
+				ModSel		<= "110"; -- "110" ONE constant
+				Bus2Dst		<= "01";  -- "01"  ALUBus2Dst
 			when 112 to 127 =>
 				-- 0111XXYY XOR src, dst
 				ALU_OP		<= "011"; -- "011" XOR
 				F_Store		<= '1';
-				Bus2Dst		<= "01"; -- "01" ALUBus2Dst
+				Bus2Dst		<= "01";  -- "01"  ALUBus2Dst
 			when 192 to 207 =>
 				-- 1100XXYY CMP src, dst
 				ALU_OP		<= "100"; -- "100" SUB
 				ALU_Cin		<= F_Carry;
+				F_Store		<= c0;
 			when 128 to 191 | 208 to 239 =>
 				--   operand		ALU	Cin	Bus2Dst
 				-- 1000xxxx ADD 	000	0		01
@@ -212,8 +212,7 @@ begin
 				ALU_OP(0)	<= irop(4) and (irop(5) xnor irop(4)) and nrop(6);
 				ALU_Cin		<= irop(5) and F_Carry;
 				F_Store		<= c0;
-				Bus2Dst(1)	<= '0';
-				Bus2Dst(0)	<= c0;
+				Bus2Dst		<= '0' & c0;
 			when 240 to 255 =>
 				-- 1111xxxx MOV
 				Bus2Dst(1)		<= (c0 and samereg);
